bd0a149b082c95b34b625a41f8d9f239a7971ea4
ParnanenJ <jiro.parnanen@student.laurea.fi>
2025-05-14 17:21:19 +0300

    Tiedostoon tallentaminen lisätty

--- Lotto_sovellus.py
+++ Lotto_sovellus.py
@@ -10,7 +10,7 @@ def plgenerator(alku, loppu, numeroita,):
         lottorivi.append(arvottunum)
     return sorted(lottorivi)
 
-def llgenerator(alku, loppu, numeroita):
+#def llgenerator(alku, loppu, numeroita):
     lisanumerot = []
     while len(lisanumerot) != numeroita:
         arvottunum = random.randint(alku, loppu)
@@ -27,11 +27,26 @@ def useampipeli(rivienmaara):
             lottorivit[f"Rivi {i}"] = plgenerator(1, 40, 7)
     elif lottopeli == 2:
         for i in range(1, rivienmaara + 1):
-            lottorivit[f"Rivi {i}"] = plgenerator(1, 48, 6), llgenerator(1, 5, 1)
+            lottorivit[f"Rivi {i}"] = plgenerator(1, 48, 6), plgenerator(1, 5, 1)
     elif lottopeli == 3:
         for i in range(1, rivienmaara + 1):
-            lottorivit[f"Rivi {i}"] = plgenerator(1, 50, 5), llgenerator(1, 12, 2)
+            lottorivit[f"Rivi {i}"] = plgenerator(1, 50, 5), plgenerator(1, 12, 2)
     return lottorivit
+
+def tallennus(tulokset):
+    while True:
+        paatos = input("Haluatko tallentaa pelirivit tiedostoon?\nKyllä = 'k' ja Ei = 'e': ")
+        if paatos == "k":
+            with open("Aiemmat_pelit.txt", "a") as tiedosto:
+                tiedosto.write(f"{tulokset}\n{'=' * 36}")
+            print("Pelirivit tallennettu!")
+            break
+        elif paatos == "e":
+            print("Tietoja ei tallennettu.")
+            break
+        else:
+            print("Syötettä ei tunnistettu.")
+
 
 # kysytään syötteet
 lottopeli = int(input("Tervetuloa lottosovellukseen!\nTässä on lottopelimme:\n 1) Lotto\n 2) Vikinglotto\n 3) Eurojackpot\nValitse lottopeli (1-3): "))
@@ -47,6 +62,7 @@ elif lottopeli == 3:
 
 # suoritetaan lottorivien teko
 if lottopeli > 0 and lottopeli < 4:
+    tulokset =f"\n{'=' * 36}\n{pelitulostus.center(36)}\n{'=' * 36}" # Alustetaan tellennukseen menevä muuttuja
     rivit= useampipeli(montalottoa)
     print(f"\n{'=' * 36}")
     print(pelitulostus.center(36))
@@ -55,11 +71,15 @@ if lottopeli > 0 and lottopeli < 4:
         if lottopeli == 2 or lottopeli == 3:  # Jos kyseessä on VikingLotto tai Eurojackpot
             pl = " ".join(f"{num:02}" for num in r[0])  # Pääpelin numerot
             ll = " ".join(f"{num:02}" for num in r[1])  # Lisänumerot
+            tulokset = f"{n}: {pl} | {ll}".center(36)   # Lisätään rivi muuttajaan tallennusta varten
             print(f"{n}: {pl} | {ll}".center(36))
         else:  # Jos tavallinen Lotto
             pl = " ".join(f"{num:02}" if num >= 10 else f" {num}" for num in r)  # Yksittäiset numerot saavat eteen tyhjän
+            tulokset += f"\n{n}: {pl}".center(36)   # Lisätään rivi muuttajaan tallennusta varten
             print(f"{n}: {pl}".center(36))
     print("=" * 36)
 else:
     print("Lottopeliä ei löytynyt :(")
+
+tallennus(tulokset)
 
