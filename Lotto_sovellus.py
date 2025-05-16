af6e0d04294008da049a60b1e50af9d3be5d2ff6
ParnanenJ <jiro.parnanen@student.laurea.fi>
2025-05-16 11:11:17 +0300

    Sovellus tehty jatkuvaksi

--- Lotto_sovellus.py
+++ Lotto_sovellus.py
@@ -10,15 +10,6 @@ def plgenerator(alku, loppu, numeroita,):
         lottorivi.append(arvottunum)
     return sorted(lottorivi)
 
-#def llgenerator(alku, loppu, numeroita):
-    lisanumerot = []
-    while len(lisanumerot) != numeroita:
-        arvottunum = random.randint(alku, loppu)
-        if arvottunum in lisanumerot:
-            continue
-        lisanumerot.append(arvottunum)
-    return sorted(lisanumerot)
-
 # Lottorivien määrä
 def useampipeli(rivienmaara):
     lottorivit = {}
@@ -33,53 +24,72 @@ def useampipeli(rivienmaara):
             lottorivit[f"Rivi {i}"] = plgenerator(1, 50, 5), plgenerator(1, 12, 2)
     return lottorivit
 
+# Tuosten tallentaminen tiedostoon nimeltä "Aiemmat_pelit.txt"
 def tallennus(tulokset):
     while True:
-        paatos = input("Haluatko tallentaa pelirivit tiedostoon?\nKyllä = 'k' ja Ei = 'e': ")
-        if paatos == "k":
+        paatos = input("Haluatko tallentaa pelirivit tiedostoon?\nKyllä = 'k' ja Ei = 'e': ").upper()
+        if paatos == "K":
             with open("Aiemmat_pelit.txt", "a") as tiedosto:
                 tiedosto.write(f"{tulokset}\n{'=' * 36}")
             print("Pelirivit tallennettu!")
             break
-        elif paatos == "e":
+        elif paatos == "E":
             print("Tietoja ei tallennettu.")
             break
         else:
             print("Syötettä ei tunnistettu.")
 
 
-# kysytään syötteet
-lottopeli = int(input("Tervetuloa lottosovellukseen!\nTässä on lottopelimme:\n 1) Lotto\n 2) Vikinglotto\n 3) Eurojackpot\nValitse lottopeli (1-3): "))
-montalottoa = int(input("Montako lottoriviä haluat luoda (1-10): "))
+while True:
+    # kysytään syötteet
+    lottopeli = int(input("Tervetuloa lottosovellukseen!\nTässä on lottopelimme:\n 1) Lotto\n 2) Vikinglotto\n 3) Eurojackpot\nValitse lottopeli (1-3): "))
+    montalottoa = int(input("Montako lottoriviä haluat luoda (1-10): "))
 
-pelitulostus = ""
-if lottopeli == 1:
-    pelitulostus = "Lotto"
-elif lottopeli == 2:
-    pelitulostus = "Vikinglotto"
-elif lottopeli == 3:
-    pelitulostus = "Eurojackot"
+    pelitulostus = ""
+    if lottopeli == 1:
+        pelitulostus = "Lotto"
+    elif lottopeli == 2:
+        pelitulostus = "Vikinglotto"
+    elif lottopeli == 3:
+        pelitulostus = "Eurojackot"
 
-# suoritetaan lottorivien teko
-if lottopeli > 0 and lottopeli < 4:
-    tulokset =f"\n{'=' * 36}\n{pelitulostus.center(36)}\n{'=' * 36}" # Alustetaan tellennukseen menevä muuttuja
-    rivit= useampipeli(montalottoa)
-    print(f"\n{'=' * 36}")
-    print(pelitulostus.center(36))
-    print("=" * 36)
-    for n, r in rivit.items():
-        if lottopeli == 2 or lottopeli == 3:  # Jos kyseessä on VikingLotto tai Eurojackpot
-            pl = " ".join(f"{num:02}" for num in r[0])  # Pääpelin numerot
-            ll = " ".join(f"{num:02}" for num in r[1])  # Lisänumerot
-            tulokset += f"\n{n}: {pl} | {ll}".center(36)   # Lisätään rivi muuttajaan tallennusta varten
-            print(f"{n}: {pl} | {ll}".center(36))
-        else:  # Jos tavallinen Lotto
-            pl = " ".join(f"{num:02}" if num >= 10 else f" {num}" for num in r)  # Yksittäiset numerot saavat eteen tyhjän
-            tulokset += f"\n{n}: {pl}".center(36)   # Lisätään rivi muuttajaan tallennusta varten
-            print(f"{n}: {pl}".center(36))
-    print("=" * 36)
-else:
-    print("Lottopeliä ei löytynyt :(")
+    # suoritetaan lottorivien teko
+    if lottopeli > 0 and lottopeli < 4:
+        tulokset =f"\n{'=' * 36}\n{pelitulostus.center(36)}\n{'=' * 36}" # Alustetaan tellennukseen menevä muuttuja
+        rivit= useampipeli(montalottoa)
+        print(f"\n{'=' * 36}")
+        print(pelitulostus.center(36))
+        print("=" * 36)
+        for n, r in rivit.items():
+            if lottopeli == 2 or lottopeli == 3:  # Jos kyseessä on VikingLotto tai Eurojackpot
+                pl = " ".join(f"{num:02}" for num in r[0])  # Pääpelin numerot
+                ll = " ".join(f"{num:02}" for num in r[1])  # Lisänumerot
+                tulokset += f"\n{n}: {pl} | {ll}".center(36)   # Lisätään rivi muuttajaan tallennusta varten
+                print(f"{n}: {pl} | {ll}".center(36))
+            else:  # Jos tavallinen Lotto
+                pl = " ".join(f"{num:02}" if num >= 10 else f" {num}" for num in r)  # Yksittäiset numerot saavat eteen tyhjän
+                tulokset += f"\n{n}: {pl}".center(36)   # Lisätään rivi muuttajaan tallennusta varten
+                print(f"{n}: {pl}".center(36))
+        print("=" * 36)
+    else:
+        print("Lottopeliä ei löytynyt :(")
+        continue
 
-tallennus(tulokset)
+    tallennus(tulokset)
+    while True:
+        jatko = input("Kiitos pelaamisesta!\nJatka pelaamista = 'j'\nNäytä tallennetut rivit = 'n'\nLopeta = 'l'\nValinta: ").upper()
+        if jatko == "J":
+            print("Jatketaan pelaamista.")
+            break
+        elif jatko == "N":
+            print("Tallennetut rivit:\n")
+            with open("Aiemmat_pelit.txt") as tiedosto:
+                print(tiedosto.read())
+        elif jatko == "L":
+            break
+        else:
+            print("Syötettä ei tunnistettu.")
 
+    if jatko == "L":
+        print("Lopetetaan sovellus!")
+        break
